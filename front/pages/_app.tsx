7a0dc8605596e59217f004b4333f293a3a207b87
윤시준 <webcodur@gmail.com>
2023-03-19 17:31:25 +0900

    dark mode basic settings added

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,8 +1,35 @@
-import type { AppProps } from 'next/app'
-import AppLayout from '../component/Applayout'
+import type { AppProps } from "next/app";
+import AppLayout from "../component/Applayout";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { useState } from "react";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    mode: string;
+  }
+}
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${(props) =>
+      props.theme.mode === "dark" ? "#111" : "#EEE"};
+    color: ${(props) => (props.theme.mode === "dark" ? "#EEE" : "#111")};
+  }
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <AppLayout>
-    <Component {...pageProps} />
-  </AppLayout>
+  const [themeMode, setThemeMode] = useState("light");
+  const toggleTheme = () => {
+    setThemeMode(themeMode === "light" ? "dark" : "light");
+  };
+
+  return (
+    <ThemeProvider theme={{ mode: themeMode }}>
+      <GlobalStyle />
+      <button onClick={toggleTheme}>Theme</button><br/>
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </ThemeProvider>
+  );
 }
